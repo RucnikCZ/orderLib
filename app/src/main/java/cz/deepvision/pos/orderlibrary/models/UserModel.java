15f0aedafef8a40257067febd54319f737ffa918
Lukas <lukas.outly@gmail.com>
2020-05-22 09:33:03 +0200

    User Model update

--- app/src/main/java/cz/deepvision/pos/orderlibrary/models/UserModel.java
+++ app/src/main/java/cz/deepvision/pos/orderlibrary/models/UserModel.java
@@ -1,6 +1,8 @@
 package cz.deepvision.pos.orderlibrary.models;
 
 
+import java.util.ArrayList;
+
 import cz.deepvision.pos.orderlibrary.utils.EnumUtil;
 
 public class UserModel {
@@ -13,6 +15,7 @@ public class UserModel {
     private String externalID;
     private Double startMoney;
     private String domain;
+    private ArrayList<String> branches;
 
     public String getFirstName() {
         return firstName;
@@ -84,5 +87,13 @@ public class UserModel {
 
     public void setDomain(String domain) {
         this.domain = domain;
+    }
+
+    public ArrayList<String> getBranches() {
+        return branches;
+    }
+
+    public void setBranches(ArrayList<String> branches) {
+        this.branches = branches;
     }
 }
